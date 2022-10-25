485d0b7574d7e99a97afb831e3e2a08b0401f369
licask8 <elielson.elrick@gmail.com>
2022-10-25 11:34:13 -0300

    iniciando estrutura visual

---
+++ ignite-teams/App.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from 'react-native';
+
+import { ThemeProvider} from 'styled-components';
+import theme from './src/theme'
+
+// carregamento das fonts
+import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+
+import { Groups } from "@screens/Groups"
+import { Loading } from '@components/Loading';
+
+
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
+
+  return (
+   <ThemeProvider theme={theme}>
+    <StatusBar 
+     barStyle='light-content'
+     backgroundColor='transparent'
+     translucent
+    />
+     { fontsLoaded ? <Groups /> : <Loading />}
+   </ThemeProvider> 
+  );
+}
+
